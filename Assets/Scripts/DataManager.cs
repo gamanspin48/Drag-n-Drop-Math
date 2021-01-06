993381800f746aa13a7236f99fe5f2a8e6fce969
SyifaUfis <nurasysyifaufissudur@student.telkomuniversity.ac.id>
2021-01-06 09:05:00 +0700

    add scenes

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{   
+    [HideInInspector]
+    public string questionType;
+    public static DataManager Instance { get; private set; } // static singleton
+    void Awake() {
+         DontDestroyOnLoad(this.gameObject);
+         if (Instance == null) { Instance = this;  }
+         else { Destroy(gameObject); }
+         // Cache references to all desired variables
+        //  player= FindObjectOfType<Player>();
+     }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void OpenQuestion(string type){
+       DataManager.Instance.questionType = type;
+       SceneManager.LoadScene(sceneName:"Question");
+   }
+}
