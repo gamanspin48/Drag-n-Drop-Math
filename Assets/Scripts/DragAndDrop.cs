bc242195407747f12e18a7ed658fc81860fee633
SyifaUfis <nurasysyifaufissudur@student.telkomuniversity.ac.id>
2021-01-09 10:08:01 +0700

    drag and drop apel piring

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+
+    private bool isDragging;
+    public string tagParent;
+    public bool isOnParent;
+    public Vector3 originalPosition;
+
+    public void Start(){
+        originalPosition = gameObject.transform.position;
+    }
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+        if (!isOnParent){
+            gameObject.transform.position = originalPosition;
+        }
+    }
+
+    void Update()
+    {
+        if (isDragging) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {   
+        isOnParent = col.gameObject.tag == tagParent;  
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {   
+        if (col.gameObject.tag == tagParent)
+            isOnParent = false;
+    }
+}
